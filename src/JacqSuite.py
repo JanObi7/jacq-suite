2e7642e7b318de767fef39ab16e503899945f3d3
Jan Obermüller <jan.obermueller@iav.de>
2024-10-29 14:31:20 +0100

    first version with pyside

--- src/JacqSuite.py
+++ src/JacqSuite.py
@@ -1,7 +1,10 @@
 import numpy as np
 import cv2 as cv
 import json
+import sys, os
 
+from PySide6 import QtWidgets, QtGui
+from PySide6.QtWidgets import QFileDialog, QTabWidget, QGridLayout
 import JacqScan, JacqPattern, JacqCard, JacqWeave
 
 class Fabric:
@@ -123,3 +126,166 @@ class Fabric:
     self.loadPattern()
     texture = JacqWeave.render(self.pattern, self.config["pattern"]["dk"], self.config["pattern"]["ds"])
     cv.imwrite(self.path+"/textures/texture.png", texture)
+
+
+#############################################################################
+class MainWindow(QtWidgets.QMainWindow):
+
+  def __init__(self):
+    super().__init__()
+
+    # data
+    if config["last_project"]:
+      self.fabric = Fabric(config["last_project"])
+    else:
+      self.fabric = None
+
+    self.resize(640,400)
+    self.setWindowTitle("JacqSuite")
+    self.setWindowIcon(QtGui.QIcon(os.path.join(os.path.dirname(__file__), 'JacqSuite.ico')))
+
+    self.tab1 = QtWidgets.QWidget()
+    self.tab1.setLayout(QtWidgets.QGridLayout())
+
+    tabs = QTabWidget()
+    tabs.addTab(self.tab1, "Ausschnitte")
+    self.setCentralWidget(tabs)
+
+
+    actionNew = QtGui.QAction("neu ...", self)
+    actionNew.setStatusTip("ein neues Projekt anlegen")
+    actionNew.triggered.connect(self.newProject)
+
+    actionOpen = QtGui.QAction("öffnen ...", self)
+    actionOpen.setStatusTip("ein bestehendes Projekt zur Bearbeitung öffnen")
+    actionOpen.triggered.connect(self.openProject)
+
+    actionClose = QtGui.QAction("schließen", self)
+    actionClose.setStatusTip("aktuelles Projekt schließen")
+    actionClose.triggered.connect(self.closeProject)
+
+    actionEditPattern = QtGui.QAction("bearbeiten", self)
+    actionEditPattern.setStatusTip("Muster bearbeiten")
+    actionEditPattern.triggered.connect(self.fabric.editPattern)
+
+    actionRenderPattern = QtGui.QAction("ausgeben", self)
+    actionRenderPattern.setStatusTip("Muster in Datei ausgeben")
+    actionRenderPattern.triggered.connect(self.fabric.renderPattern)
+
+    actionInitPattern = QtGui.QAction("zurücksetzen", self)
+    actionInitPattern.setStatusTip("Muster löschen")
+    actionInitPattern.triggered.connect(self.fabric.initPattern)
+
+    actionGenerateCards = QtGui.QAction("ausgeben", self)
+    actionGenerateCards.setStatusTip("Karten erzeugen")
+    actionGenerateCards.triggered.connect(self.fabric.generateCards)
+
+    actionRenderTexture = QtGui.QAction("ausgeben", self)
+    actionRenderTexture.setStatusTip("Textur in Datei ausgeben")
+    actionRenderTexture.triggered.connect(self.fabric.renderTexture)
+
+
+    menubar = self.menuBar()
+    project = QtWidgets.QMenu("Projekt")
+    project.addAction(actionOpen)
+    project.addAction(actionClose)
+    menubar.addMenu(project)
+
+    pattern = QtWidgets.QMenu("Muster")
+    pattern.addAction(actionEditPattern)
+    pattern.addAction(actionRenderPattern)
+    pattern.addSeparator()
+    pattern.addAction(actionInitPattern)
+    menubar.addMenu(pattern)
+
+    cards = QtWidgets.QMenu("Karten")
+    cards.addAction(actionGenerateCards)
+    menubar.addMenu(cards)
+
+    menu = QtWidgets.QMenu("Texturen")
+    menu.addAction(actionRenderTexture)
+    menubar.addMenu(menu)
+
+    self.updateViews()
+
+  def updateViews(self):
+    if self.fabric:
+      self.setWindowTitle("JacqSuite - " + self.fabric.path)
+
+      layout = self.tab1.layout()
+      for i in reversed(range(layout.count())):
+        widget = layout.itemAt(i).widget()
+        layout.removeWidget(widget)
+        widget.setParent(None)
+
+      for i in range(len(self.fabric.config["scans"])):
+        scan = self.fabric.config["scans"][i]
+        name = QtWidgets.QLabel(scan["filename"])
+        layout.addWidget(name, i,1)
+        ranges = QtWidgets.QLabel(f"K: {scan["kmin"]} .. {scan["kmax"]}, S: {scan["smin"]} .. {scan["smax"]}")
+        layout.addWidget(ranges, i, 2)
+        digit = QtWidgets.QPushButton("Digitalisieren")
+        layout.addWidget(digit, i,3)
+        edit = QtWidgets.QPushButton("Bearbeiten")
+        layout.addWidget(edit, i, 4)
+        remove = QtWidgets.QPushButton("Löschen")
+        layout.addWidget(remove, i, 5)
+
+    else:
+      self.setWindowTitle("JacqSuite")
+
+      layout = self.tab1.layout()
+      for i in reversed(range(layout.count())):
+        widget = layout.itemAt(i).widget()
+        layout.removeWidget(widget)
+        widget.setParent(None)
+
+
+  def newProject(self):
+    path = QFileDialog.getExistingDirectory(self, "Ordner auswählen", ".", QFileDialog.ShowDirsOnly | QFileDialog.DontResolveSymlinks)
+    print(path)
+    self.updateViews()
+    
+  def openProject(self):
+    path = QFileDialog.getExistingDirectory(self, "Ordner auswählen", "./data", QFileDialog.ShowDirsOnly | QFileDialog.DontResolveSymlinks)
+    if path:
+      config["last_project"] = path
+      saveAppConfig()
+      self.fabric = Fabric(path)
+      self.updateViews()
+
+  def closeProject(self):
+    if self.fabric:
+      config["last_project"] = None
+      saveAppConfig()
+      self.fabric.saveConfig()
+      self.fabric = None
+      self.updateViews()
+    
+
+#############################################################################
+config = {
+  "last_project": None
+}
+
+def loadAppConfig():
+  global config
+
+  if os.path.exists("JacqSuite.config"):
+    with open("JacqSuite.config", "r") as file:
+      config = json.load(file)
+  else:
+    saveAppConfig()
+  
+def saveAppConfig():
+  with open("JacqSuite.config", "w") as file:
+    json.dump(config, file, indent=2)
+
+
+if __name__ == '__main__':
+  loadAppConfig()
+  app = QtWidgets.QApplication(sys.argv)
+  win = MainWindow()
+  win.show()
+  app.exec()
+  saveAppConfig()

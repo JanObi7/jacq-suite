4db92c25f5c1da357b2b1f09b798ff1497b3e209
Jan Obermüller <jan_obermueller@web.de>
2025-06-03 20:55:20 +0200

    default value

--- src/settings.py
+++ src/settings.py
@@ -1,12 +1,12 @@
 import json, os
 
-def readSetting(name):
+def readSetting(name, default=None):
   if os.path.exists("config.json"):
     with open("config.json", "r") as file:
       config = json.load(file)
       if name in config:
         return config[name]
-  return None
+  return default
   
 def writeSetting(name, value):
   if os.path.exists("config.json"):
